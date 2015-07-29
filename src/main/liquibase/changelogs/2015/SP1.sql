27fa53d2aeb4b5b986247a15a438d41d4d93fde1
Ravi Sen <Ravi.Sen@cdk.com>
2015-07-29 22:05:35 +0530

    SP Test

--- src/main/liquibase/changelogs/2015/SP1.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
---changeset senr:002 runOnChange:true endDelimiter:#
-DROP PROCEDURE IF EXISTS sayHelloWorld;
-#
-CREATE PROCEDURE sayHelloWorld()
-BEGIN
-    SELECT 'Hello World From a MySql Database!';
-END
-#
